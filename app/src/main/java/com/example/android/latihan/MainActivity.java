5feaff298c93a8eed21cdb64454e960c004d11c0
nauvalafif <mr.nauvalafif@s.itb.ac.id>
2018-01-03 21:10:16 +0700

    gggg

--- app/src/main/java/com/example/android/latihan/MainActivity.java
+++ app/src/main/java/com/example/android/latihan/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.latihan;
 
 import java.text.NumberFormat;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -43,8 +47,15 @@ public class MainActivity extends AppCompatActivity {
         String krim = Boolean.toString(checkedorNot(R.id.krim_checkbox));
         String coklat = Boolean.toString(checkedorNot(R.id.coklat_checkbox));
         String priceMessage = createOrderSummary(price, krim, coklat);
-        displayMessage(priceMessage);
-
+        // displayMessage(priceMessage);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Pesanan dari Bp/Ibu" + getName());
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        /**
         if ((checkedorNot(R.id.krim_checkbox)) && (checkedorNot(R.id.coklat_checkbox))) {
             displayPic(R.drawable.kopi_krim_coklat);
         } else if (checkedorNot(R.id.krim_checkbox)) {
@@ -54,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             displayPic(R.drawable.kopi);
         }
+         */
     }
 
     public int calculatePrice(int quantity) {
@@ -62,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String createOrderSummary(int harga, String krim, String coklat) {
-        return "Name : Daffa Daneswara" + '\n' + "Dikeki krim? " + krim + '\n' + "Dikeki coklat? " + coklat + '\n' + "Quantity : " + quantity + '\n' + "Total : " + NumberFormat.getCurrencyInstance().format(harga) + '\n' + "Thank You !";
+        return "Name : " + getName() + '\n' + "Dikeki krim? " + krim + '\n' + "Dikeki coklat? " + coklat + '\n' + "Quantity : " + quantity + '\n' + "Total : " + NumberFormat.getCurrencyInstance().format(harga) + '\n' + "Thank You !";
 
     }
     /**
@@ -80,18 +92,24 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
 
     private boolean checkedorNot(int id) {
         CheckBox cekboks = (CheckBox) findViewById(id);
         return cekboks.isChecked();
     }
-
+/**
     private void displayPic(int idpic) {
         ImageView gambarkopi = (ImageView) findViewById(R.id.gambar_kopi);
         gambarkopi.setImageResource(idpic);
+    }
+ */
+
+    private String getName() {
+        EditText namefield = (EditText) findViewById(R.id.your_name);
+        return namefield.getText().toString();
     }
 }
